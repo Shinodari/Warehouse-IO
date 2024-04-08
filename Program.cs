4126eba4a395b593d37059336de212c0ab74759d
Shinodari <jugkapun@C13-339>
2024-04-08 14:20:30 +0700

    feat(style): init

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Warehouse_IO
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
