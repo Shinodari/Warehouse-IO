6291dfbc1ecc006f3ff23546790b07a6bf011028
Hibino02 <zolo_atm@msn.com>
2024-04-09 15:08:12 +0700

    build: Global Variable CONNECTION_STRING & .NET Framework Update & MySQL.Data install ref #1

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,22 +8,27 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Warehouse_IO.Properties
-{
-
-
+namespace Warehouse_IO.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("server=127.0.0.1;uid=root;pwd=12345678;database=warehouse-io")]
+        public string CONNECTION_STRING {
+            get {
+                return ((string)(this["CONNECTION_STRING"]));
             }
         }
     }
