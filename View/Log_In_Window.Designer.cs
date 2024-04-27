bf90d47caab902a26cc46a582106c11b86d7cbc5
Hibino Haleyluja <zolo_atm@msn.com>
2024-04-27 20:59:43 +0700

    feat(Log_In):new #4

---
+++ View/Log_In_Window.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Warehouse_IO
+{
+    partial class Log_In_Window
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.logInButt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 24);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 69);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(174, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // logInButt
+            // 
+            this.logInButt.Location = new System.Drawing.Point(232, 40);
+            this.logInButt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.logInButt.Name = "logInButt";
+            this.logInButt.Size = new System.Drawing.Size(92, 29);
+            this.logInButt.TabIndex = 2;
+            this.logInButt.Text = "Log In";
+            this.logInButt.UseVisualStyleBackColor = true;
+            this.logInButt.Click += new System.EventHandler(this.LogIn_Click);
+            // 
+            // Log_In_Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.logInButt);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Log_In_Window";
+            this.Size = new System.Drawing.Size(364, 110);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button logInButt;
+    }
+}
---
+++ View/Log_In_Window.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Warehouse_IO.Authentication;
+using Warehouse_IO.Common;
+
+namespace Warehouse_IO
+{
+    public partial class Log_In_Window : UserControl
+    {
+        public event EventHandler LoggedIn;
+        public Log_In_Window()
+        {
+            InitializeComponent();          
+        }
+
+        private void LogIn_Click(object sender, EventArgs e)
+        {
+            string u = textBox1.Text;
+            if (IsValid(u))
+            {
+                Login log = new Login(textBox1.Text);
+                log.Authenticate(textBox2.Text);
+                if (log.IsComplete)
+                {
+                    User userlog = new User(u);
+                    Global.isadmin = userlog.IsAdmin;
+                    LoggedIn?.Invoke(this, EventArgs.Empty);
+                }
+                else
+                {
+                    MessageBox.Show(this,"Invalid");
+                }
+            }
+            else
+            {
+                MessageBox.Show(this, "Username atleast 6 and no space");
+            }
+        }
+        bool IsValid(string u)
+        {
+            Regex regex = new Regex(@"^\S.{4,}\S$", RegexOptions.Compiled);
+            return regex.IsMatch(u);
+        }
+
+        private void Click_Cerate_User(object sender, EventArgs e)
+        {
+            Create_User create = new Create_User();
+            create.Show();
+        }
+    }
+}
