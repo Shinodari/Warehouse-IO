eab83c9a2a1d4ad24606d92db2193be44431fed1
Hibino Haleyluja <zolo_atm@msn.com>
2024-04-29 10:41:42 +0700

    chore:

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ namespace Warehouse_IO
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
