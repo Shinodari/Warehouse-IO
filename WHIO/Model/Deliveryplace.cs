08c2dd936f63719de3b8f403141252a21621ce5d
Hibino Haleyluja <zolo_atm@msn.com>
2024-05-10 12:59:01 +0700

    feat(Deliveryplace): Create Delieryplace Back-End in Model ref: #9

---
+++ WHIO/Model/Deliveryplace.cs
@@ -0,0 +1,168 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Warehouse_IO.WHIO.Model
+{
+    class Deliveryplace
+    {
+        int id;
+        public int ID { get { return id; }set { id = value; } }
+        string name;
+        public string Name { get { return name; } set { name = value; } }
+
+        static string connstr = Settings.Default.CONNECTION_STRING;
+
+        void CheckAndUpdateField(string columnName, string value)
+        {
+            MySqlConnection conn = null;
+            try
+            {
+                conn = new MySqlConnection(connstr);
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    string check = $"SELECT * FROM deliveryplace WHERE {columnName} = @value";
+                    cmd.CommandText = check;
+                    cmd.Parameters.AddWithValue("@value", value);
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            id = Convert.ToInt32(reader["ID"]);
+                            name = reader["Name"].ToString();
+                        }
+                    }
+                }
+            }
+            catch (MySqlException e) { }
+            finally
+            {
+                if (conn != null && conn.State != ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+
+        public Deliveryplace(int id)
+        {
+            CheckAndUpdateField("ID", id.ToString());
+        }
+        public Deliveryplace(string name)
+        {
+            this.name = name;
+        }
+
+        public bool Create()
+        {
+            MySqlConnection conn = null;
+            try
+            {
+                conn = new MySqlConnection(connstr);
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    string insert = "INSERT INTO deliveryplace (ID, Name) VALUES (NULL, @name)";
+                    cmd.CommandText = insert;
+                    cmd.Parameters.AddWithValue("@name", name);
+                    cmd.ExecuteNonQuery();
+                }
+                return true;
+            }
+            catch (MySqlException e)
+            {
+                return false;
+            }
+            finally
+            {
+                if (conn != null && conn.State != ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+        public bool Change()
+        {
+            MySqlConnection conn = null;
+            try
+            {
+                conn = new MySqlConnection(connstr);
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    string update = "UPDATE deliveryplace SET Name = @name WHERE ID = @id ";
+                    cmd.CommandText = update;
+                    cmd.Parameters.AddWithValue("@name", name);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+                return true;
+            }
+            catch (MySqlException e)
+            {
+                return false;
+            }
+            finally
+            {
+                if (conn != null && conn.State != ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+        public bool Remove()
+        {
+            MySqlConnection conn = null;
+            try
+            {
+                conn = new MySqlConnection(connstr);
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    string delete = "DELETE FROM deliveryplace WHERE ID = @id";
+                    cmd.CommandText = delete;
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+                return true;
+            }
+            catch (MySqlException e)
+            {
+                return false;
+            }
+            finally
+            {
+                if (conn != null && conn.State != ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+
+        public static List<Deliveryplace> GetDeliveryplaceList()
+        {
+            MySqlConnection conn = null;
+            List<Deliveryplace> deliveryplaceList = new List<Deliveryplace>();
+            try
+            {
+                conn = new MySqlConnection(connstr);
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    string updateArrayList = "SELECT * FROM deliveryplace";
+                    cmd.CommandText = updateArrayList;
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id = Convert.ToInt32(reader["ID"]);
+                            Deliveryplace item = new Deliveryplace(id);
+                            deliveryplaceList.Add(item);
+                        }
+                    }
+                }
+            }
+            catch (MySqlException e) { }
+            finally
+            {
+                if (conn != null && conn.State != ConnectionState.Closed)
+                    conn.Close();
+            }
+            return deliveryplaceList;
+        }
+    }
+}
